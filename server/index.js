ea2f5c3ec729eda2f5a298448843e6d8abd875a5
lahlaliwassim2 <wlahlali34@gmail.com>
2023-03-20 12:49:30 +0000

    importation des dependences et ajouter les dossier statiques

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from "express"
+import bodyParser from "body-parser"
+import mongoose from "mongoose"
+import corse from "cors"
+import dotenv from "dotenv"
+import multer from "multer"
+import helmet from "helmet"
+import morgan from "morgan"
+import path from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+dotenv.config()
