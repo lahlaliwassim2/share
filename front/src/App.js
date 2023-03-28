f148475334f58a32f3991af5fcd827694998e139
lahlaliwassim2 <wlahlali34@gmail.com>
2023-03-28 18:45:58 +0000

    cofiguration des routes

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,18 @@
-import './App.css'
-
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "screens/homePage";
+import LoginPage from "screens/loginPage";
+import ProfilePage from "screens/profilePage";
 function App() {
+
   return (
     <div className="app">
-          app
+         <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+         </BrowserRouter>
     </div>
   );
 }
