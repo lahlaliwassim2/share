21719bcb84afbd20571dc412d1aa8d20c350431a
lahlaliwassim2 <wlahlali34@gmail.com>
2023-04-04 22:23:50 +0000

    ajouter la fonction de mode qui permet de naviger entre les diferens modes qui existe dans l application en utulisant createTheme themeSettings et ajoute l objet theme qui contient les diferent styles pour les modes

--- front/src/App.js
+++ front/src/App.js
@@ -2,17 +2,29 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "screens/homePage";
 import LoginPage from "screens/loginPage";
 import ProfilePage from "screens/profilePage";
+import { useMemo } from "react"
+import { useSearchParams } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "theme";
+import { useSelector } from "react-redux";
+import state from "state";
 
 function App() {
-
+const mode = useSelector((state) => state.mode)
+const theme = useMemo(()=> createTheme(themeSettings(mode), [mode]))
   return (
     <div className="app">
          <BrowserRouter>
+         <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        
           <Routes>
             <Route path="/" element={<LoginPage />} />
             <Route path="/home" element={<HomePage />} />
             <Route path="/profile/:userId" element={<ProfilePage />} />
           </Routes>
+          </ThemeProvider>
          </BrowserRouter>
     </div>
   );
