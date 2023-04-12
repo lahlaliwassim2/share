566ac1adfa4633e25c8687d82d5a4d1bd3463fc8
lahlaliwassim2 <wlahlali34@gmail.com>
2023-04-12 00:45:27 +0000

    integrer le composant de news dans le composant global

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -6,7 +6,7 @@ import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
 import AdvertWidget from "scenes/widgets/AdvertWidget";
 import FriendListWidget from "scenes/widgets/FriendListWidget";
-
+import NewsWidget from "scenes/widgets/NewsWidget";
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
   const { _id, picturePath } = useSelector((state) => state.user);
@@ -23,6 +23,7 @@ const HomePage = () => {
       >
         <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
           <UserWidget userId={_id} picturePath={picturePath} />
+          <NewsWidget />
         </Box>
         <Box
           flexBasis={isNonMobileScreens ? "42%" : undefined}
